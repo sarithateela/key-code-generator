0f9dc45687a7ecb179478579cbbe1154a5a44bc3
sarithateela <147496073+sarithateela@users.noreply.github.com>
2024-05-27 21:12:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('keydown', function(event) {
+    const keyName = event.key;
+    const keyCode = event.keyCode;
+
+    document.getElementById('key').textContent = keyName;
+    document.getElementById('key-code').textContent = keyCode;
+});
